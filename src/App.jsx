6f1c1bd941e2a147ce6f90e464fab1c80c2cad01
Shekhar Khedekar <shekharkhedekar@Shekhars-MacBook-Pro.local>
2022-04-13 11:05:13 -0700

    pictorial congress vote

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import GroupMeetingSpot from "./components/GroupMeetingSpot";
+import PictorialCongressVotes from "./components/PictorialCongressVotes";
 import { Home } from "./components/Home";
 import { Resume } from "./components/Resume";
 
@@ -14,6 +15,10 @@ const App = () => {
         <Route
           path="/group-meeting-spot"
           element={<GroupMeetingSpot />}
+        ></Route>
+        <Route
+          path="/pictorial-congress-votes"
+          element={<PictorialCongressVotes />}
         ></Route>
       </Routes>
     </Router>
