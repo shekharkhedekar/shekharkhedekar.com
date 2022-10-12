429ac5a2848293fac63f64680b03630a44c0c65d
shekharkhedekar <shekhar.khedekar@gmail.com>
2022-10-12 12:57:54 -0700

    more styled components progress

--- src/components/Home/components/Work.tsx
+++ src/components/Home/components/Work.tsx
@@ -1,53 +1,62 @@
-import React from "react";
-import { Element as ScrollElement, Link as ScrollLink } from "react-scroll";
-import styled from "styled-components";
-import { SIZES } from "../../GlobalStyle";
+import React from 'react';
+import { Element as ScrollElement } from 'react-scroll';
+import styled from 'styled-components';
+import { SIZES } from '../../GlobalStyle';
 
-import data from "../data.json";
-import { Divider } from "./Divider";
-import { Link } from "./Link";
+import data from '../data.json';
+import {
+    Categories,
+    Category,
+    CategoryContent,
+    CategoryHeader,
+    CategoryItem,
+    CategoryMeta,
+    CategoryTitle,
+} from './Categoory';
+import { Divider } from './Divider';
+import { Link } from './Link';
 
 const WorkWrap = styled.div`
-  width: ${SIZES.maxWidth};
-  position: relative;
+    width: ${SIZES.maxWidth};
+    position: relative;
 `;
 
 const MainHeader = styled.h2``;
 
 const ResumeLink = styled(Link)`
-  position: absolute;
-  top: 75px;
-  right: 0;
+    position: absolute;
+    top: 75px;
+    right: 0;
 `;
 
 export const Work: React.FC = () => {
-  return (
-    <ScrollElement name="sk-work" id="sk-work">
-      <WorkWrap>
-        <MainHeader>Work</MainHeader>
-        <ResumeLink href="/resume" title="resume">
-          View Resume
-        </ResumeLink>
+    return (
+        <ScrollElement name="sk-work" id="sk-work">
+            <WorkWrap>
+                <MainHeader>Work</MainHeader>
+                <ResumeLink href="/resume" title="resume">
+                    View Resume
+                </ResumeLink>
 
-        <div className="sk-work-categories">
-          {data.work.map((w) => (
-            <div className="sk-work-category">
-              <h3 className="sk-work-header">{w.name}</h3>
-              <Divider />
-              {w.items.map((i) => (
-                <div className="sk-work-item">
-                  <div className="sk-work-item-title">{i.title}</div>
-                  <div className="sk-work-item-description">
-                    {i.description}
-                  </div>
-                  <div className="sk-work-item-time">{i.time}</div>
-                  <div className="sk-work-item-content">{i.content}</div>
-                </div>
-              ))}
-            </div>
-          ))}
-        </div>
-      </WorkWrap>
-    </ScrollElement>
-  );
+                <Categories>
+                    {data.work.map((w) => (
+                        <Category>
+                            <CategoryHeader>{w.name}</CategoryHeader>
+                            <Divider />
+                            {w.items.map((i) => (
+                                <CategoryItem>
+                                    <CategoryTitle>{i.title}</CategoryTitle>
+                                    <CategoryMeta>{i.description}</CategoryMeta>
+                                    <CategoryMeta>{i.time}</CategoryMeta>
+                                    <CategoryContent>
+                                        {i.content}
+                                    </CategoryContent>
+                                </CategoryItem>
+                            ))}
+                        </Category>
+                    ))}
+                </Categories>
+            </WorkWrap>
+        </ScrollElement>
+    );
 };
--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,115 +1,56 @@
-import React, { useEffect } from "react";
-import WebFont from "webfontloader";
-import { Helmet } from "react-helmet";
-import styled from "styled-components";
+import React, { useEffect } from 'react';
+import WebFont from 'webfontloader';
+import { Helmet } from 'react-helmet';
+import styled from 'styled-components';
 
-import data from "./data.json";
-import { Expander } from "../Expander";
+import data from './data.json';
+import { Expander } from '../Expander';
 
-import { StringOrLink } from "./components/StringOrLink";
-import { Intro } from "./components/Intro";
-import { Divider } from "./components/Divider";
-import { GlobalStyle } from "../GlobalStyle";
-import { Work } from "./components/Work";
+import { StringOrLink } from './components/StringOrLink';
+import { Intro } from './components/Intro';
+import { Divider } from './components/Divider';
+import { GlobalStyle } from '../GlobalStyle';
+import { Work } from './components/Work';
+import { Play } from './components/Play';
 
 export const Home = () => {
-  const loadWebFonts = (cb: () => void) => {
-    try {
-      WebFont.load({
-        google: {
-          families: ["Arvo", "Source Sans Pro"],
-        },
-        active: cb,
-        inactive: cb,
-      });
-    } catch (e) {
-      console.error("Error loading web fonts.");
-    }
-  };
+    const loadWebFonts = (cb: () => void) => {
+        try {
+            WebFont.load({
+                google: {
+                    families: ['Arvo', 'Source Sans Pro'],
+                },
+                active: cb,
+                inactive: cb,
+            });
+        } catch (e) {
+            console.error('Error loading web fonts.');
+        }
+    };
 
-  useEffect(() => {
-    loadWebFonts(() => {});
-  }, []);
+    useEffect(() => {
+        loadWebFonts(() => {});
+    }, []);
 
-  const ContentWrap = styled.div`
-    height: 100%;
-  `;
+    const ContentWrap = styled.div`
+        height: 100%;
+    `;
 
-  return (
-    <>
-      <Helmet>
-        <title>Shekhar Khedekar</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Helmet>
-      <GlobalStyle />
-      <ContentWrap>
-        <Intro />
-        <Work />
-
-        <div id="sk-play">
-          <div className="sk-work-wrap sk-font-dependency">
-            <h2 className="sk-main-header">Play</h2>
-
-            <div className="sk-work-categories">
-              {data.play.map((p) => (
-                <div className="sk-work-category sk-play-category">
-                  <h3 className="sk-work-item-title sk-play-title">
-                    {p.title}
-                  </h3>
-                  <Divider />
-                  <div className="sk-work-item-content">
-                    {p.description &&
-                      p.description.map((d) => <StringOrLink content={d} />)}
-                  </div>
-                  {p.subItems?.map((t) => (
-                    <Expander
-                      label={
-                        `${t.title} ` +
-                        ("location" in t ? `(${t.location})` : "")
-                      }
-                    >
-                      <>
-                        {"subtitle" in t &&
-                          t.subtitle.map((s) => (
-                            <>
-                              <div className="sk-work-item-description">
-                                {s.title}
-                              </div>
-
-                              <div className="sk-work-item-content">
-                                {"accolades" in s && (
-                                  <>
-                                    <div className="sk-work-item-description">
-                                      Achievements
-                                    </div>
-                                    {s.accolades?.map((a) => (
-                                      <li>
-                                        <StringOrLink content={a} />
-                                      </li>
-                                    ))}
-                                  </>
-                                )}
-                              </div>
-                            </>
-                          ))}
-                        {"href" in t && (
-                          <a
-                            href={t.href}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                          >
-                            website&raquo;
-                          </a>
-                        )}
-                      </>
-                    </Expander>
-                  ))}
-                </div>
-              ))}
-            </div>
-          </div>
-        </div>
-      </ContentWrap>
-    </>
-  );
+    return (
+        <>
+            <Helmet>
+                <title>Shekhar Khedekar</title>
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1"
+                />
+            </Helmet>
+            <GlobalStyle />
+            <ContentWrap>
+                <Intro />
+                <Work />
+                <Play />
+            </ContentWrap>
+        </>
+    );
 };
