8cfe6256a09e826b344a0f6059ca8bc5506cb40d
shekharkhedekar <shekhar.khedekar@gmail.com>
2023-02-12 13:25:47 -0800

    refactor and style

--- src/components/VCardGenerator/index.tsx
+++ src/components/VCardGenerator/index.tsx
@@ -4,60 +4,20 @@ import csv from 'csvtojson';
 
 import { Header } from '../common/Header';
 import { Helmet } from 'react-helmet';
+import { About } from './About';
+import { Inputs } from './Inputs';
+import { Row } from './types';
+import { VCards } from './VCards';
+import { jsonArraysToObjects } from './utils';
 
-export type Row = Record<string, string>;
-
-const generateVCard = (row: Row, additionalInfo: string) => {
-    const rowOutput = [
-        'BEGIN:VCARD',
-        'VERSION:4.0',
-        `FN:${row.firstName} ${row.lastName}`,
-        `N:${row.lastName};${row.firstName}`,
-    ];
-
-    if (row.email) {
-        rowOutput.push(`EMAIL:${row.email}`);
-    }
-
-    if (row.phone) {
-        rowOutput.push(`TEL:${row.phone}`);
-    }
-
-    if (additionalInfo || row.info) {
-        const info = [];
-        if (additionalInfo) info.push(additionalInfo);
-        if (row.info) info.push(row.info);
-        rowOutput.push(`ORG:${info.join(' - ')}`);
-    }
-
-    rowOutput.push('END:VCARD');
-
-    return rowOutput.join('\n');
-};
-
-const jsonArraysToObjects = (input: string[][], additionalInfo: string) => {
-    const [keys, ...rows] = input;
-    const output: Row[] = [];
-
-    rows.forEach((row) => {
-        const rowObj: Row = {};
-        row.forEach((value, i) => {
-            rowObj[keys[i]] = value;
-        });
-        rowObj.vcard = generateVCard(rowObj, additionalInfo);
-        output.push(rowObj);
-    });
-
-    return output;
-};
+const DEFAULT_CSV = `firstName,lastName,email,phone,info
+Bob,Loblaw,bob.loblaw@gmail.com,510-555-9999,My Lawyer
+`;
 
 export const VCardGenerator: FC = () => {
     // TODO validation
     const [json, setJson] = useState<Row[]>([]);
-    const [value, setValue] =
-        useState<string>(`firstName,lastName,email,phone,info
-Bob,Loblaw,bob.loblaw@gmail.com,510-555-9999,My Lawyer
-`);
+    const [value, setValue] = useState<string>(DEFAULT_CSV);
     const [additionalInfo, setAdditionalInfo] = useState('');
 
     const parseCsv = useCallback(() => {
@@ -103,101 +63,27 @@ Bob,Loblaw,bob.loblaw@gmail.com,510-555-9999,My Lawyer
                 />
             </Helmet>
             <Header>
-                <div style={{ display: 'flex', gap: '0.5rem' }}>
-                    <img src="vcard.png" width={31} height={24} alt="logo" />
-                    Generate VCards from CSV
-                </div>
-            </Header>
-            <div style={{ padding: '1rem' }}>
-                <h2>About</h2>
-                <p>
-                    Have a contact list that you want to convert into contacts
-                    for your phone? Paste a CSV file below with the{' '}
-                    <strong>firstName</strong>, <strong>lastName</strong>,{' '}
-                    <strong>email</strong>, <strong>phone</strong>, and{' '}
-                    <strong>info</strong> headers, and download the VCards
-                    below. After downloading them, open the VCF file with your
-                    contacts app.
-                </p>
-                <p>
-                    This site does not store any of your information, it's all
-                    handled in the browser without making any network requests.
-                    See the source code{' '}
-                    <a
-                        href="https://github.com/shekharkhedekar/shekharkhedekar.com/tree/master/src/components/VCardGenerator"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        here
-                    </a>
-                    .
-                </p>
-                <h2>CSV Input</h2>
-                <div>Paste or type your CSV here:</div>
-                <textarea
-                    value={value}
-                    rows={25}
-                    style={{ width: '100%' }}
-                    onChange={onTextAreaChange}
-                />
-                <label>
-                    Additional Info to add to all contacts:
-                    <input
-                        placeholder="E.g. name of your child's school etc"
-                        type="text"
-                        style={{ marginLeft: '0.5rem' }}
-                        value={additionalInfo}
-                        onChange={onInputChange}
-                    />
-                </label>
-
-                <h2>VCards</h2>
-                <a
-                    style={{
-                        color: 'blue',
-                        display: 'block',
-                        marginBottom: '1rem',
-                    }}
-                    href={`data:text/plain;charset=utf-8,${encodeURIComponent(
-                        json.map((row) => row.vcard).join('\n\n')
-                    )}`}
-                    download={`contacts.vcf`}
-                >
-                    Download All
-                </a>
-
-                <div
+                <h1
                     style={{
                         display: 'flex',
-                        gap: '1rem',
-                        flexWrap: 'wrap',
+                        gap: '0.5rem',
+                        alignItems: 'center',
+                        fontSize: '1.33rem',
                     }}
                 >
-                    {json.map((row) => (
-                        <pre
-                            style={{
-                                background: '#eee',
-                                padding: '1rem',
-                                borderRadius: '10px',
-                                border: '1px solid #ccc',
-                                margin: 0,
-                            }}
-                        >
-                            <div>{row.vcard}</div>
-                            <div style={{ marginTop: '1rem' }}>
-                                <a
-                                    style={{ color: 'blue' }}
-                                    href={`data:text/plain;charset=utf-8,${encodeURIComponent(
-                                        row.vcard
-                                    )}`}
-                                    download={`${row.firstName} ${row.lastName}.vcf`}
-                                >
-                                    Download
-                                </a>
-                            </div>
-                        </pre>
-                    ))}
-                </div>
+                    <img src="vcard.png" width={31} height={24} alt="logo" />
+                    Generate VCards from CSV
+                </h1>
+            </Header>
+            <div style={{ padding: '1rem' }}>
+                <About />
+                <Inputs
+                    csvValue={value}
+                    onCsvValueChange={onTextAreaChange}
+                    additionalInfo={additionalInfo}
+                    onAdditionalInfoChange={onInputChange}
+                />
+                <VCards json={json} />
             </div>
         </>
     );
