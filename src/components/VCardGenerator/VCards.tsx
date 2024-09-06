91522c9d4708c2e9cb5c8bba0ef5c0806de74c77
shekharkhedekar <shekhar.khedekar@gmail.com>
2024-09-06 15:54:13 -0700

    add zip generation, resume update

--- src/components/VCardGenerator/VCards.tsx
+++ src/components/VCardGenerator/VCards.tsx
@@ -1,35 +1,41 @@
 import { FC, MouseEvent } from 'react';
 import { CsvJson } from './types';
+import { BlobWriter, TextReader, ZipWriter } from '@zip.js/zip.js';
 
 export interface VCardsProps {
     json: CsvJson;
 }
+
+async function getZipFileBlob(json: CsvJson) {
+    const zipWriter = new ZipWriter(new BlobWriter('application/zip'));
+    await Promise.all(
+        json.map(({ vcard, firstName, lastName }) => {
+            if (firstName === '' && lastName === '') {
+                return Promise.resolve();
+            }
+
+            const name = `contacts/${firstName} ${lastName}.vcf`;
+            return zipWriter.add(name, new TextReader(vcard));
+        })
+    );
+
+    return zipWriter.close();
+}
+
+function downloadFile(blob: Blob, filename: string) {
+    const link = document.createElement('a');
+    link.download = filename;
+    link.href = URL.createObjectURL(blob);
+    link.click();
+    document.body.removeChild(link);
+}
+
 export const VCards: FC<VCardsProps> = ({ json }) => {
-    const downloadAllVcardsSeparately = (e: MouseEvent<HTMLAnchorElement>) => {
+    const downloadAllVcards = async (e: MouseEvent<HTMLAnchorElement>) => {
         e.preventDefault();
-        var link = document.createElement('a');
-
-        link.setAttribute('download', '');
-        link.style.display = 'none';
-
-        document.body.appendChild(link);
-
-        for (var i = 0; i < json.length; i++) {
-            const row = json[i];
-            link.setAttribute(
-                'href',
-                `data:text/plain;charset=utf-8,${encodeURIComponent(row.vcard)}`
-            );
-            link.setAttribute(
-                'download',
-                `${row.firstName} ${row.lastName}.vcf`
-            );
-            link.click();
-        }
-
-        document.body.removeChild(link);
+        const blob = await getZipFileBlob(json);
+        downloadFile(blob, 'all-contacts.zip');
     };
-
     return (
         <>
             <h2>VCards</h2>
@@ -39,15 +45,15 @@ export const VCards: FC<VCardsProps> = ({ json }) => {
                     href={`data:text/plain;charset=utf-8,${encodeURIComponent(
                         json.map((row) => row.vcard).join('\n\n')
                     )}`}
-                    download={`contacts.vcf`}
+                    download={`all-contacts.vcf`}
                 >
                     Download All-in-one Contact
                 </a>
 
                 <a
                     style={{ color: 'blue' }}
-                    href="#"
-                    onClick={downloadAllVcardsSeparately}
+                    href="/"
+                    onClick={downloadAllVcards}
                 >
                     Download All Individual Contacts
                 </a>
